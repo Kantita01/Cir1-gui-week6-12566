3e8435d93db59ed3c1f090a21544e037a5610168
Kantita01 <kantita5069@gmail.com>
2023-06-20 12:41:24 +0700

    first commit

---
+++ bin/dart_week6.dart
@@ -0,0 +1,10 @@
+void main(){
+  int x = 10;
+  double y = 5;
+  print("Arithemetic x + y : $x + $y = ${x + y}");
+  print("Arithemetic x - y : $x - $y = ${x - y}");
+  print("Arithemetic x * y : $x * $y = ${x * y}");
+  print("Arithemetic x / y : $x / $y = ${x / y}");
+  print("Arithemetic x ~/ y : $x ~/ $y = ${x ~/ y}");
+  print("Arithemetic x  % : $x % $y = ${x % y}");
+}
